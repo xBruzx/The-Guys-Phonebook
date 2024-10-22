921a8fcaf837fde989b750a82fe44209d4b55bb2
Alec Szczechowicz <65929878+xBruzx@users.noreply.github.com>
2024-10-22 15:54:12 -0500

    Holly Molly github please stop deleting my files.
    This is whack yo!

---
+++ Contact.h
@@ -0,0 +1,121 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Contact 
+{
+    //attributes
+    private:
+        string firstName;
+        string lastName;
+        string email;
+        string address;
+        string phoneNum;
+        int areaCode;
+
+    //methods
+    public:
+        //getters
+        string getFirstName() const;
+        string getLastName() const;
+        string getEmail() const;
+        string getAddress() const;
+        string getPhoneNum() const;
+        int getAreaCode() const;
+
+        //setters
+        void setFirstName(string);
+        void setLastName(string);
+        void setEmail(string);
+        void setAddress(string);
+        void setPhoneNum(string);
+        void setAreaCode(int);
+
+        //overloaded operators
+        bool operator==(const Contact& right_comparison)
+        {
+            return this->lastName == right_comparison.lastName;
+        }
+        bool operator<(const Contact& right_comparison)
+        {
+            return this->lastName < right_comparison.lastName;
+        }
+        bool operator>(const Contact& right_comparison)
+        {
+            return this->lastName > right_comparison.lastName;
+        }
+        
+        friend ostream& operator<<(ostream& os, Contact& cntc)
+        {
+            os << "(" << cntc.firstName << ", " << cntc.lastName << ", " << cntc.email << ", " <<
+            cntc.address << ", " << cntc.phoneNum << ", " << cntc.areaCode << ")";
+            return os;
+        }
+};
+
+//getters
+string Contact::getFirstName() const
+{
+    return this->firstName;
+}
+
+string Contact::getLastName() const
+{
+    return this->lastName;
+}
+
+string Contact::getEmail() const
+{
+    return this->email;
+}
+
+string Contact::getAddress() const
+{
+    return this->address;
+}
+
+string Contact::getPhoneNum() const
+{
+    return this->phoneNum;
+}
+
+int Contact::getAreaCode() const
+{
+    return this->areaCode;
+}
+
+//setters
+void Contact::setFirstName(string firstName)
+{
+    this->firstName = firstName;
+}
+
+void Contact::setLastName(string lastName)
+{
+    this->lastName = lastName;
+}
+
+void Contact::setEmail(string email)
+{
+    this->email = email;
+}
+
+void Contact::setAddress(string address)
+{
+    this->address = address;
+}
+
+void Contact::setPhoneNum(string phoneNum)
+{
+    this->phoneNum = phoneNum;
+}
+
+void Contact::setAreaCode(int areaCode)
+{
+    this->areaCode;
+}
+
+#endif
